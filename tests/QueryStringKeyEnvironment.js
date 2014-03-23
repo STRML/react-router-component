4bb335ce1f689ca1398ea637da1386a176c02728
Andrey Popp <8mayday@gmail.com>
2014-03-23 19:57:20 +0400

    Tests and fixes for QuerystringKeyEnvironment.

---
+++ tests/QueryStringKeyEnvironment.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var assert                    = require('assert');
+var QuerystringKeyEnvironment = require('../lib/environment/QuerystringKeyEnvironment');
+
+describe('QuerystringKeyEnvironment', function() {
+
+  var env;
+
+  beforeEach(function() {
+    env = new QuerystringKeyEnvironment('key');
+  });
+
+  afterEach(function() {
+    window.history.pushState({}, '', '/');
+  });
+
+  it('updates the corresponding key in querystring on setPath', function() {
+    env.setPath('/x', {});
+    assert.equal(location.search, '?key=x');
+  });
+
+  it('returns a corresponding key from querystring via getPath', function() {
+    assert.equal(env.getPath(), '/');
+    window.history.pushState({}, '', '/?key=y');
+    assert.equal(env.getPath(), '/y');
+  });
+
+});
