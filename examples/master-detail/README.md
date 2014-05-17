0c518e274ff1c4fc33aa0051fabdc873603458d8
Andrey Popp <8mayday@gmail.com>
2014-05-17 14:45:56 +0400

    examples: master-detail

---
+++ examples/master-detail/README.md
@@ -0,0 +1,5 @@
+# Example: master-detail
+
+We treat URL as an encoding for props. Router decodes props from URL and pass it
+further to handler. Handler doesn't know from where it gets props so after the
+Router everything is pure React.
---
+++ examples/master-detail/index.js
@@ -0,0 +1,62 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react');
+var ReactRouter = require('../../');
+var Locations = ReactRouter.Locations;
+var Location = ReactRouter.Location;
+var Link = ReactRouter.Link;
+
+var Master = React.createClass({
+
+  render: function() {
+    var links = [1, 2, 3, 4, 5].map((item) =>
+      <li>
+        <Link href={`/${item}`}>Item {item}</Link>
+      </li>
+    );
+    return <ul>{links}</ul>;
+  }
+});
+
+var Detail = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <p>Detailed info for item: {this.props.item}</p>
+      </div>
+    );
+  }
+});
+
+var Page = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <Master />
+        {this.props.item && <Detail item={this.props.item} />}
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <h1>Master-detail with react-router-component</h1>
+        <Locations hash>
+          <Location path="/" handler={Page} />
+          <Location path="/:item" handler={Page} />
+        </Locations>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(App(), document.getElementById('host'));
